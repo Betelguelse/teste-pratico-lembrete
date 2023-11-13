895a43bdddf5cc8b5f35243abc34090f23946f92
Betelguelse <eddiecp1501@gmail.com>
2023-11-13 17:34:01 -0300

    backup

--- testes-unitarios/insercao.test.js
+++ testes-unitarios/insercao.test.js
@@ -1,5 +1,5 @@
 const insercao = require ('./insercao');
 
-test('Inserindo nome "Lembrar de tomar remédio" + data "13/01/2023" exibe um novo lembrete criado' , () => {
+test('Inserindo nome "Lembrar de tomar remédio" + data "13/11/2023" exibe um novo lembrete criado' , () => {
     expect();
 })
